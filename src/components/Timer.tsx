99b32de35fbd0696a9fa7d36fb171a0ecfef2c98
quarterwire <watcher.alex0411@gmail.com>
2025-09-30 13:27:36 +0300

    changed progressbar

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -1,6 +1,5 @@
 import { type EggType } from '../assets/constants'
 import Countdown, { zeroPad } from 'react-countdown'
-import BentProgressBar from '../ui/Progressbar'
 
 interface TimerProps {
   egg: EggType
@@ -13,7 +12,6 @@ const Timer = ({ egg }: TimerProps) => {
     minutes,
     seconds,
     completed,
-    total,
   }: {
     minutes: number
     seconds: number
@@ -24,32 +22,27 @@ const Timer = ({ egg }: TimerProps) => {
       return <div>Done!</div>
     }
 
-    const elapsedSeconds = totalSeconds - Math.ceil(total / 1000)
-    const progressPercent = (elapsedSeconds / totalSeconds) * 100
-
     return (
       <div className="flex flex-col items-center gap-2">
-        <div className="text-xl font-semibold">
+        <div className="text-5xl font-semibold text-amber-900">
           {zeroPad(minutes)}:{zeroPad(seconds)}
         </div>
-        <BentProgressBar
-          progress={progressPercent} // 0 → 100%
-          imageUrl="/icon.png"
-          width="64"
-          height="64"
-        />
+        <img src="./timer.svg" className="animate-bouncer w-80"></img>
       </div>
     )
   }
 
   return (
-    <div>
-      <h1>Time left</h1>
-      <Countdown
-        precision={3}
-        renderer={renderer}
-        date={Date.now() + totalSeconds * 1000}
-      />
+    <div className="mx-auto flex flex-col gap-20 justify-center h-full bg-[url('/background.jpg')] bg-center relative">
+      <div className="absolute inset-0 bg-yellow-200/80 z-0"></div>
+      <div className="z-10 gap-30 flex flex-col">
+        <h1 className="text-center">Time left</h1>
+        <Countdown
+          precision={3}
+          renderer={renderer}
+          date={Date.now() + totalSeconds * 1000}
+        />
+      </div>
     </div>
   )
 }
